aaa936615f4b32315a49fee8616390a58758ce7c
hurricane1988 <jianpingniu88@gmail.com>
2025-01-26 14:41:28 +0800

    update the nodepool_types.go

--- api/v1alpha1/nodepool_types.go
+++ api/v1alpha1/nodepool_types.go
@@ -28,14 +28,68 @@ type NodePoolSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of NodePool. Edit nodepool_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// NodeSelector is used to dynamically select nodes for the pool based on labels
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+
+	// Nodes is an optional list of node names in the pool
+	Nodes []string `json:"nodes,omitempty"`
+
+	// Architecture specifies the node architecture (e.g., amd64, arm64)
+	Architecture []string `json:"architecture,omitempty"`
+
+	// OperatingSystem specifies the operating system of the nodes (e.g., linux, windows)
+	OperatingSystem []string `json:"operatingSystem,omitempty"`
+
+	// OSImage specifies the OS image (e.g., Ubuntu 20.04)
+	OSImage []string `json:"osImage,omitempty"`
+
+	// KernelVersion specifies the version of the node's kernel (e.g., 5.4.0-42-generic)
+	KernelVersion []string `json:"kernelVersion,omitempty"`
+
+	// KubeletVersion specifies the version of kubelet installed on the node
+	KubeletVersion []string `json:"kubeletVersion,omitempty"`
+
+	// CpuVendor specifies the CPU vendor (e.g., Intel, AMD)
+	CPUVendor []string `json:"cpuVendor,omitempty"`
+}
+
+// NodeDetail contains the information about nodes with specific properties
+type NodeDetail struct {
+	Architecture    string `json:"architecture,omitempty"`
+	OperatingSystem string `json:"operatingSystem,omitempty"`
+	OSImage         string `json:"osImage,omitempty"`
+	KernelVersion   string `json:"kernelVersion,omitempty"`
+	KubeletVersion  string `json:"kubeletVersion,omitempty"`
+	CPUVendor       string `json:"cpuVendor,omitempty"`
+	Count           int    `json:"count"`
+}
+
+// NodePoolCapacity defines the overall resource capacity of the NodePool
+type NodePoolCapacity struct {
+	CPU              string `json:"cpu"`
+	EphemeralStorage string `json:"ephemeral-storage"`
+	Hugepages1Gi     string `json:"hugepages-1Gi"`
+	Hugepages2Mi     string `json:"hugepages-2Mi"`
+	Memory           string `json:"memory"`
+	Pods             string `json:"pods"`
 }
 
 // NodePoolStatus defines the observed state of NodePool.
 type NodePoolStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// AvailableNodes contains the list of nodes in the pool
+	AvailableNodes []string `json:"availableNodes,omitempty"`
+
+	// NodeDetails stores nodes categorized by architecture, OS, etc.
+	NodeDetails map[string]NodeDetail `json:"nodeDetails,omitempty"`
+
+	// Capacity defines the resource capacity of the NodePool
+	Capacity NodePoolCapacity `json:"capacity,omitempty"`
+
+	// Nodes stores the current state of each node (Schedulable, Unschedulable)
+	Nodes map[string]string `json:"nodes,omitempty"`
 }
 
 // +kubebuilder:object:root=true
